35b5e9d0af1ea1c15d2dfd9e086ac3671105df76
Anuken <arnukren@gmail.com>
2017-02-09 11:14:22 -0500

    "Better" bases, fixed textures

--- core/src/io/anuke/novi/entities/enemies/GunBase.java
+++ core/src/io/anuke/novi/entities/enemies/GunBase.java
@@ -1,23 +1,27 @@
 package io.anuke.novi.entities.enemies;
 
-import com.badlogic.gdx.math.Vector2;
-
 import io.anuke.novi.world.Material;
 
 public class GunBase extends Base{
-
+	
 	@Override
 	void generateBlocks(){
 		for(int x = 0;x < size;x ++){
 			for(int y = 0;y < size;y ++){
 				blocks[x][y].setMaterial(Material.ironblock);
 				
-				if(Vector2.dst(x, y, size/2f, size/2f) > size/2.2f) blocks[x][y].setMaterial(Material.turret);
-				if(Math.random() < 0.1) blocks[x][y].setMaterial(Material.bigturret);
+				blocks[size/2][0].setMaterial(Material.turret);
+				blocks[size/2][size-1].setMaterial(Material.turret);
+				blocks[0][size/2-1].setMaterial(Material.turret);
+				blocks[size-1][size/2-1].setMaterial(Material.turret);
 				
-				if(Math.random() < 0.05) blocks[x][y].setMaterial(Material.dronemaker);
+				blocks[size-1][size-1].setMaterial(Material.bigturret);
+				blocks[0][size-1].setMaterial(Material.bigturret);
+				blocks[size-1][0].setMaterial(Material.bigturret);
+				blocks[0][0].setMaterial(Material.bigturret);
+				
+				blocks[size/2][size/2-1].setMaterial(Material.dronemaker);
 			}
 		}
-		
 	}
 }
